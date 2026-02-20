c714d006d5b0c0079bcad6f16c367bea7b02c4d2
Barbara-Mase <mase.barbara@gmail.com>
2026-02-20 16:28:24 +0000

    Sauvegarde projet fini

---
+++ projet-guide-blog-dev/connexion.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Chargement des variables d'environnement
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+$db = new PDO(
+    $connexionString,
+    $user,
+    $password
+);
+
+?>
